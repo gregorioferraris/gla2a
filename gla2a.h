53a32e97113c511a67cd1e0c03670a731f166fb7
gregorioferraris <ferraris.gregorio@gmail.com>
2025-07-12 21:54:46 +0200

    Update gla2a.h

--- gla2a.h
+++ gla2a.h
@@ -1,31 +1,42 @@
 #ifndef GLA2A_H
 #define GLA2A_H
 
-#include <lv2/core/lv2.h> // Include standard LV2 headers, if needed by other parts of your .h
+#include <lv2/core/lv2.h>
 
-// Definizione dell'URI del plugin (DEVE corrispondere a gla2a.ttl e gla2a.cpp)
-#define GLA2A_URI "http://moddevices.com/plugins/mod-devel/gla2a"
+// Definizione dell'URI del plugin.
+#define GLA2A_URI "http://moddevices.com/plugins/mod-devel/gla2a" // URI specifico per GLA2A
 
-// Enum degli indici delle porte
-// Questi indici DEVONO corrispondere esattamente agli indici in gla2a.ttl
+// Definizione dell'URI della GUI (se ci sarà, altrimenti usa lo stesso URI del plugin)
+#define GLA2A_GUI_URI "http://moddevices.com/plugins/mod-devel/gla2a_ui"
+
+// Enum degli indici delle porte del plugin (stessi del GLA3A per coerenza, solo nome URI cambia)
 typedef enum {
     GLA2A_PEAK_REDUCTION = 0,
     GLA2A_GAIN = 1,
     GLA2A_METER = 2,
-    GLA2A_LIMITER = 3,
-    GLA2A_HI_FREQ = 4,
-    GLA2A_OUTPUT_RMS = 5,
-    GLA2A_GAIN_REDUCTION_METER = 6,
-    GLA2A_BYPASS = 7,         // NUOVO
-    GLA2A_MS_MODE_ACTIVE = 8, // NUOVO
-    GLA2A_AUDIO_IN_L = 9,
-    GLA2A_AUDIO_IN_R = 10,
-    GLA2A_AUDIO_OUT_L = 11,
-    GLA2A_AUDIO_OUT_R = 12
+    GLA2A_BYPASS = 3,
+    GLA2A_MS_MODE_ACTIVE = 4,
+    GLA2A_RATIO_MODE = 5,
+    GLA2A_SC_LP_ON = 6,
+    GLA2A_SC_LP_FREQ = 7,
+    GLA2A_SC_LP_Q = 8,
+    GLA2A_SC_HP_ON = 9,
+    GLA2A_SC_HP_FREQ = 10,
+    GLA2A_SC_HP_Q = 11,
+    GLA2A_OUTPUT_RMS = 12,
+    GLA2A_GAIN_REDUCTION_METER = 13,
+    GLA2A_AUDIO_IN_L = 14,
+    GLA2A_AUDIO_IN_R = 15,
+    GLA2A_AUDIO_OUT_L = 16,
+    GLA2A_AUDIO_OUT_R = 17
 } GLA2A_PortIndex;
 
-// Puoi aggiungere qui altre definizioni o forward declarations se necessario
-// Ad esempio, la definizione della struttura Gla2a potrebbe andare qui,
-// ma per ora la lasciamo in .cpp per semplicità e per evitare dipendenze reciproche.
+// Enum per le modalità di ratio (per chiarezza nel codice C++)
+typedef enum {
+    GLA2A_RATIO_3_TO_1 = 0,
+    GLA2A_RATIO_6_TO_1 = 1,
+    GLA2A_RATIO_9_TO_1 = 2,
+    GLA2A_RATIO_LIMIT  = 3
+} GLA2A_RatioMode;
 
 #endif // GLA2A_H
